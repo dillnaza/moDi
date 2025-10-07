0d3d55f2686946909ca38a6ead5b3a74c12d4f36
Dilnaza <dilnazbaidakhanova@gmail.com>
2025-10-07 15:26:56 +0500

    add

---
+++ src/main/java/moDi/MoDiApplication.java
@@ -0,0 +1,13 @@
+package moDi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moDi/moDi/MoDiApplicationTests.java
@@ -0,0 +1,13 @@
+package moDi.moDi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
